244ba90cb08a62cd6edc5cd55477a630ccc79981
Mubarik Osman <zamier372@gmail.com>
2026-04-23 21:06:10 +0300

    refactor: update firebaseAdmin and improve admin routes

--- app/exams/[id]/page.tsx
+++ app/exams/[id]/page.tsx
@@ -13,8 +13,6 @@ import {
 
 // --- React PDF Imports ---
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Set up the PDF.js worker (CDN is the safest method for Next.js Turbopack)
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
